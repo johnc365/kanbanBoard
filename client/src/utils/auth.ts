2b0957191051ad9f3c877709c9ddc9e9cec6defa
johnc365 <johnrcaldwell123@gmail.com>
2024-11-11 18:32:59 -0600

    added the client side code to implement the token

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,55 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    if (token) {
+      try {
+        return jwtDecode<JwtPayload>(token);
+      } catch (error) {
+        return null;
+      }
+    }
+    return null; // Return null if no token exists
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded: JwtPayload = jwtDecode(token);
+      if (decoded.exp) {
+        const currentTime = Date.now() / 1000;
+        return decoded.exp < currentTime;
+      }
+      return false;
+    } catch (error) {
+      return true;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
   }
 }
 
